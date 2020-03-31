08d310f8aea5c5c8af82db2231098c1172821fe5
Olia1011 <olgamelnyk10111989@gmail.com>
2020-03-31 13:33:39 +0300

    New com

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// const text = "Hello npm";
+var func = function func(test) {
+  console.log(test);
+};
+
+func(test);
---
+++ src/index.js
@@ -0,0 +1,7 @@
+// const text = "Hello npm";
+
+const func = (test) => {
+    console.log(test)
+}
+
+func(test);
